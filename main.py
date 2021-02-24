f42361978e37c082274083dc008ac18820fbae19
chet2chet <karolproksa@gmail.com>
2021-02-24 22:49:10 +0100

    working menu

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print('Welcome to the Hangsnake game \n sss.. it\'s really just a hangman game written in python')
-print('asd')
+print('Welcome to the Hangsnake game \nsss.. it\'s really just a hangman game written in python. \nKeep it lowkey, it\'s a sssecret.')
+print('\nNow.. Do you want to play?')
+
+play = input('y/n \n')
+
+
+def run():
+    print('Ssso we playing? It\'s my pleasssure..') 
+
+if play == 'y' or play == 'Y' or play == 'yes' or play == 'Yes' or play == 'YES':
+    run()
+else:
+    print('We ain\'t playing? Your losssss..')  
